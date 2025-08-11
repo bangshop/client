5fff561a964ee6e5b354a9f24c9f02b88081de1d
pradeepzen <62941406+pradeepzen@users.noreply.github.com>
2025-08-11 18:35:14 +0530

    fix: Export auth service from firebase config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,20 +1,19 @@
 // src/firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth"; // 1. ADD THIS IMPORT
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAEIo9gg9wAi95xN3Uag3sfBA7r8m9ZlhI",
-    authDomain: "mywebapp-fe0c2.firebaseapp.com",
-    projectId: "mywebapp-fe0c2",
-    storageBucket: "mywebapp-fe0c2.appspot.com",
-    messagingSenderId: "209013926177",
-    appId: "1:209013926177:web:c2b1a788d86b84677ff660",
-    measurementId: "G-5YR6P325RL"
+  apiKey: "AIzaSyAEIo9gg9wAi95xN3Uag3sfBA7r8m9ZlhI",
+  authDomain: "mywebapp-fe0c2.firebaseapp.com",
+  projectId: "mywebapp-fe0c2",
+  storageBucket: "mywebapp-fe0c2.appspot.com",
+  messagingSenderId: "209013926177",
+  appId: "1:209013926177:web:c2b1a788d86b84677ff660",
+  measurementId: "G-5YR6P325RL"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize and export Firestore
 export const db = getFirestore(app);
+export const auth = getAuth(app); // 2. ADD THIS EXPORT
